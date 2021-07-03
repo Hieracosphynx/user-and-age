e3707f608d703b99aeac3e064540c3158e8db496
Hieracosphynx <cortezmiguel05@gmail.com>
2021-07-02 19:45:35 -0700

    Used portal for error modal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
-  return (
-    <div>
-      <div className={classes.backdrop} onClick={props.errorModal} />
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.errorModal} />;
+  };
+
+  const Overlay = (props) => {
+    return (
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -17,7 +22,24 @@ const ErrorModal = (props) => {
           <Button onClick={props.errorModal}>O.K</Button>
         </footer>
       </Card>
-    </div>
+    );
+  };
+
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop errorModal={props.errorModal} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <Overlay
+          title={props.title}
+          message={props.message}
+          errorModal={props.errorModal}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </>
   );
 };
 
